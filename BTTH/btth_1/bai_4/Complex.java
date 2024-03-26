3e21f0edc775c0d78d26bc76b7e6d8a9e3f3c466
DuongMinh <minhdtn@uniultra.xyz>
2024-03-26 16:59:49 +0700

    bai th bai 4

--- BTTH/btth_1/bai_4/Complex.java
+++ BTTH/btth_1/bai_4/Complex.java
@@ -1,3 +1,82 @@
-public class Complex {
+import java.util.Scanner;
 
+public class Complex {
+    private double soThuc;
+    private double soAo;
+    private static Scanner scanner = new Scanner(System.in);
+
+    // contruction
+
+    public Complex() {
+        this.soThuc = 0;
+        this.soAo = 0;
+    }
+
+    public Complex(double st, double sa) {
+        this.soThuc = st;
+        this.soAo = sa;
+    }
+
+    public void setSoThuc(double a) {
+        this.soThuc = a;
+    }
+
+    public void setSoAo(double b) {
+        this.soAo = b;
+    }
+
+    public double getSoThuc() {
+        return this.soThuc;
+    }
+
+    public double getSoAo() {
+        return this.soAo;
+    }
+
+    // function
+
+    public void Nhap() {
+        System.out.print("Vui long nhap vao phan thuc: ");
+        setSoThuc(scanner.nextDouble());
+        System.out.print("Vui long nhap vao phan ao: ");
+        setSoAo(scanner.nextDouble());
+    }
+
+    public void Xuat() {
+        if (getSoAo() < 0) {
+            System.out.println(getSoThuc() + " - " + Math.abs(getSoAo()) + "*i");
+        } else {
+            System.out.println(getSoThuc() + " + " + getSoAo() + "*i");
+        }
+        System.out.println();
+    }
+
+    public Complex cong(Complex b) {
+        double thuc = getSoThuc() + b.getSoThuc();
+        double ao = getSoAo() + b.getSoAo();
+        return new Complex(thuc, ao);
+    }
+
+    public Complex tru(Complex b) {
+        double thuc = getSoThuc() - b.getSoThuc();
+        double ao = getSoAo() - b.getSoAo();
+        return new Complex(thuc, ao);
+    }
+
+    public Complex nhan(Complex b) {
+        double thuc = getSoThuc() * b.getSoThuc() - getSoAo() * b.getSoAo();
+        double ao = getSoThuc() * b.getSoAo() + b.getSoThuc() * getSoAo();
+        return new Complex(thuc, ao);
+
+    }
+
+    public Complex chia(Complex b) {
+        double thuc = (getSoThuc() * b.getSoThuc() + getSoAo() * b.getSoAo())
+                / (b.getSoThuc() * b.getSoThuc() + b.getSoAo() * b.getSoAo());
+
+        double ao = (b.getSoThuc() * getSoAo() - getSoThuc() * b.getSoAo())
+                / (b.getSoThuc() * b.getSoThuc() + b.getSoAo() * b.getSoAo());
+
+        return new Complex(thuc, ao);
+    }
 }
--- BTTH/btth_1/bai_4/Main_btth1_4.java
+++ BTTH/btth_1/bai_4/Main_btth1_4.java
@@ -1,5 +1,22 @@
-public class Main_btth1_4 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main_btth1_4 {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Complex cp1 = new Complex();
+        Complex cp2 = new Complex();
+
+        // nhap
+        cp1.Nhap();
+        cp2.Nhap();
+
+        // xuat
+        cp1.Xuat();
+
+        // cong
+        Complex cp3 = cp1.cong(cp2);
+        System.out.println("Ket qua phep tinh tren la: ");
+        cp3.Xuat();
     }
 }
